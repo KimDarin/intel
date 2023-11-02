0af20256942e648027939324fff41d81c049a85d
EMBEDDED <ekfls73@naver.com>
2023-11-02 11:08:09 +0900

    23.11.02-1

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -8,8 +8,10 @@ private:         //접근 지시자(외부 참조 불가)
 
 public:          //접근 지시자(외부 참조 가능)   
     int x, y;    //멤버 변수 선언, 객체(나 자신)의 좌표, 두 점 중 한 점은 나 자신   
-    Point(int x1 = 0, int y1 = 0) : x(x1), y(y1)     //이니셜라이져(너무 많은 것 넣지 않기)
+    Point(int x1 = 0, int y1 = 0) : x(x1), y(y1)     //이니셜라이저(너무 많은 것 넣지 않기)
         //생성자 정의(멤버 변수와 arg로 받는 변수 구별), 함수적 특성(overload, default  등) 
+        //cf. 이니셜라이저 할 때 에는 이름 같게 해도 됨.
+        //ex) (int x = 0, int y = 0) : x(x), y(y)   (O)
     {
         //x = x1; y = y1;          
     }
@@ -62,14 +64,36 @@ void printP(Point p)
 {
     printf("Point(%d, %d)\n", p.x, p.y);
 }
+
+void swap_v(int a, int b)             //call by value
+{   
+    int temp(a);
+    printf("[swap] 초기값  a = %d, 초기값  b = %d\n", a, b);
+    a = b;
+    b = temp;
+    printf("[swap] 결과값  a = %d, 결과값  b = %d\n", a, b);
+
+}
+
+void swap_p(int *a, int *b)           //call by reference
+{
+    int temp(*a);
+    printf("[swap] 초기값  a = %d, 초기값  b = %d\n", *a, *b);
+    *a = *b;
+    *b = temp;
+    printf("[swap] 결과값  a = %d, 결과값  b = %d\n", *a, *b);
+
+}
+
+
 int main()
 {
   
-    Point p1, p2(10, 10), p3(20, 30), p4;       //포인트 객체 선언, p1은 (0, 0) default값이 0이기 때문
+    Point p1, p2(10, 10), p3(20, 30), p4;        //포인트 객체 선언, p1은 (0, 0) default값이 0이기 때문
     p4 = p3;
-    double d = p1.Dist(p2);                 //기준.Dist(~까지의 거리)
+    double d = p1.Dist(p2);                          //기준.Dist(~까지의 거리)
     double d1 = p2.Dist(p3);
-    double d2 = p1.Dist(p2, p3);            //여기 있는 p1은 순전히 함수를 사용하기 위한 객체
+    double d2 = p1.Dist(p2, p3);                 //여기 있는 p1은 순전히 함수를 사용하기 위한 객체
     //p2에서 p3거리 구하는 법
     printP(p1); printP(p2); printP(p3); printP(p4);    
     printf("점 간의 계산: d(p1, p2) = %.2f, d1(p2, p3) = %.2f, d2(p2, p3) = %.2f\n", d, d1, d2);
@@ -78,9 +102,24 @@ int main()
 
     printf("두 점\n");
     printP(p2); printP(p3);
-    printf("에 의해 정의된 사각형의 꼭짓점의 좌표는\n");
+   
    /* printP(r.LOWL);  printP(r.LOWR);  printP(r.UPL);  printP(r.UPR);  */
     printP (r.GetLOWL());  printP(r.GetLOWR());  printP(r.GetUPL());  printP(r.GetUPR());
+
+    int a(10), b(20);                                                                           //변수에도 이니셜라이저 됨(C++의 새로운 대입문)
+    //a = 10; b = 20; 
+    printf("초기값  a = %d, 초기값  b = %d\n", a, b);
+
+    int a1, b1;             
+    printf("a1 = %d, b1 = %d\n", a1 = 20, b1 = 30);       //변수가 상수가 아니라면 가능
+    //printf("변수 a1 = %d, b1 = %d\n", a1(30), b1(40));       (X)    //"변수(값)"은 변수 초기화 시에만 가능     
+
+    swap_v(a, b);
+    printf(" [swap_v] a = %d, b = %d\n", a, b);                                            //call by value의 대표적인 예
+
+    swap_p(&a, &b);
+    printf(" [swap_p] a = %d, b = %d\n", a, b);
+
 }
 
 
