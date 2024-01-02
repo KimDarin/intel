8297b0549ea6565c9349db3399aa690ed48736d2
EMBEDDED <ekfls73@naver.com>
2024-01-02 16:43:03 +0900

    24.01.02

---
+++ FigureClass/FigureClass.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class Shape
+{
+public:
+	int x, y;
+	Shape(int x = 0, int y = 0) : x(x), y(y) {}
+	~Shape() {}
+};
+
+class Circle : public Shape
+{
+
+};
+
+class Rectangle : public Shape
+{
+
+};
+
+class Triangle : public Shape
+{
+
+};
+
+int main()
+{
+ 
+}
